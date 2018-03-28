d0f7129e747cd287656042c9ce293693f73dfccb
Ryan Benson <benson.ryan@gmail.com>
2018-03-28 12:58:32 -0500

    Parsing csv and testing it

--- csv.go
+++ csv.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+  "bufio"
+  "encoding/csv"
+  "os"
+  "io"
+)
+
+type CsvParser struct {
+  Data [][]string
+  File *os.File
+}
+
+func (c CsvParser) init(filename string) CsvParser {
+  f, _ := os.Open(filename)
+  c.File = f
+  return c
+}
+
+func (c CsvParser) parse() CsvParser {
+  var data [][]string
+  r := csv.NewReader(bufio.NewReader(c.File))
+  for {
+    record, err := r.Read()
+
+    if err == io.EOF {
+      break
+    }
+    data = append(data, record)
+  }
+  c.Data = data
+  return c
+}
--- csv_test.go
+++ csv_test.go
@@ -15,9 +15,11 @@ func TestConfig(t *testing.T) {
   g.Describe("CSV", func() {
 
     g.It("should parse the csv and get the data as an array", func() {
-      // c := new(csv).init().parse()
+      filename := "problems.csv"
+      c := new(CsvParser).init(filename).parse()
 
-      Ω("test").Should(Equal("list of records"))
+      var answers = [][]string{{"5+5", "10"},{"7+3", "10"},{"1+1", "2"},{"8+3", "11"},{"1+2", "3"},{"8+6", "14"},{"3+1", "4"},{"1+4", "5"},{"5+1", "6"},{"2+3", "5"},{"3+3", "6"},{"2+4", "6"},{"5+2", "7"}}
+      Ω(c.Data).Should(Equal(answers))
     })
 
   })
